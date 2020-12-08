5511d0f48faccc52a566b84cc7604d272fccc401
Rafael Ehammer <Rafael.ehammer@htl-wels.at>
2020-12-08 16:43:34 +0100

    added LoginModel

--- src/login/LoginModel.java
+++ src/login/LoginModel.java
@@ -1,4 +1,24 @@
 package login;
 
-public class LoginModel {
+public class LoginModel
+{
+    String username;
+    String password;
+
+    public LoginModel()
+    {
+        this.username = "Cisco";
+        this.password = "Cisco";
+    }
+
+    public boolean CorrectLogin (String n, String p)
+    {
+        if (n.equals(username) && p.equals(password))
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
--- src/main/MainModel.java
+++ src/main/MainModel.java
@@ -1,4 +1,6 @@
 package main;
 
-public class MainModel {
+public class MainModel
+{
+
 }
